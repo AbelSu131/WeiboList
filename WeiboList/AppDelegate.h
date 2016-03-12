546c11aadd3dcd8aa3da5a81090cb62fcc283440
AbelSu <abelsu1993@outlook.com>
2016-03-12 20:27:56 +0800

    Initial Commit

---
+++ WeiboList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiboList
+//
+//  Created by AbelSu on 16/3/12.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiboList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiboList
+//
+//  Created by AbelSu on 16/3/12.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
